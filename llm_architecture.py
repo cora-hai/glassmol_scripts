0ee24492d23ed290bf2b4ef3703d1516fde91fd0
cora-hai <cora.haiber@ruhr-uni-bochum.de>
2026-09-11 15:59:00 +0200

    same as last

--- llm_architecture.py
+++ llm_architecture.py
@@ -191,7 +191,7 @@ def main(in_data_folder, model_folder, tok_folder, data_type, num_epochs, num_co
                 label = batch['label'].to(device)
                 concept_labels = batch['concept_labels']
 
-                                # If a 1D tensor was passed, add a batch dimension to make it 2D (1, sequence_length)
+                # If a 1D tensor was passed, add a batch dimension to make it 2D (1, sequence_length)
                 if input_ids.dim() == 1:
                     input_ids = input_ids.unsqueeze(0)
                     attention_mask = attention_mask.unsqueeze(0)
@@ -201,7 +201,7 @@ def main(in_data_folder, model_folder, tok_folder, data_type, num_epochs, num_co
                     input_ids = input_ids.squeeze(1)
                     attention_mask = attention_mask.squeeze(1)
 
-                outputs = model(input_ids=input_ids.squeeze(), attention_mask=attention_mask.squeeze(), output_hidden_states=True)
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
                 pooled_output = outputs.hidden_states[-1][:,0]
 
                 outputs = ModelXtoCtoY_layer(pooled_output)
@@ -243,7 +243,7 @@ def main(in_data_folder, model_folder, tok_folder, data_type, num_epochs, num_co
                 input_ids = input_ids.squeeze(1)
                 attention_mask = attention_mask.squeeze(1)
 
-            outputs = model(input_ids=input_ids.squeeze(), attention_mask=attention_mask.squeeze(), output_hidden_states=True)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask, output_hidden_states=True)
             pooled_output = outputs.hidden_states[-1][:,0]
 
             outputs = ModelXtoCtoY_layer(pooled_output)
